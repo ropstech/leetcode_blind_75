29ef19045f6788087bb0eef90dc69edebf682979
bangerang-rob <robin.spalthoff@gmail.com>
2023-03-07 21:50:45 +0100

    modified main.py and renamed Two Sum folder

--- Longest Substring Without Repeating Characters/main.py
+++ Longest Substring Without Repeating Characters/main.py
@@ -1,19 +1,19 @@
+'''
+Optimized Sliding Window Solution
+All other solutions to the question can be found in the README.md documentation.
+'''
 
 class Solution:
     def length_of_longest_substring(s: str) -> int:
         n = len(s)
-        chars = set()
+        char_dict = {}
         i, j = 0, 0
         max_length = 0
-        while i < n and j < n:
-            if s[j] not in chars:
-                chars.add(s[j])
-                j += 1
-                max_length = max(max_length, j-i)
+        while j < n:
+            if s[j] in char_dict and i <= char_dict[s[j]]:
+                i = char_dict[s[j]] + 1
             else:
-                chars.remove(s[i])
-                i += 1
+                max_length = max(max_length, j - i + 1)
+            char_dict[s[j]] = j
+            j += 1
         return max_length
-    
-s = "abcabcbb"
-print(Solution.length_of_longest_substring(s)) 
